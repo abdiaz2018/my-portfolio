f741e7dc953d8136095299eaa3d59bfda2268ba1
Abraham <abdiaz2018@gmail.com>
2022-01-10 10:46:31 -0800

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Navbar from './navbar';
+import { Routes, Route } from 'react-router';
+import Home from './home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1 id="title">Abraham Armando Diaz</h1>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about"/>
+        </Routes>
+      </div>
+    )
+  }
 }
 
 export default App;
