17448e09e19688161b4d2850759e0a1f0e303d59
Abraham <abdiaz2018@gmail.com>
2022-01-10 10:47:03 -0800

    adding navbar

---
+++ src/navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import './navbar.css'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div id="navbar">
+                <Link class="link" to="/">Home</Link>
+                <Link class="link" to="/about">About</Link>
+            </div>
+        )
+    }
+} 
+
+export default Navbar;
