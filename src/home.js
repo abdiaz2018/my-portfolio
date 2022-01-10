5c7b2cd7bb47984b42f6fe2863fd218747494f5f
Abraham <abdiaz2018@gmail.com>
2022-01-10 10:46:48 -0800

    adding home page

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <main>
+                <h2>Hola, I'm Abraham Diaz</h2>
+            </main>
+        )
+    }
+}
+
+export default Home;
