7eacfe5fc584415c3e02d6c0ca5c801626eeb0d0
Abraham <abdiaz2018@gmail.com>
2022-01-22 11:33:54 -0800

    Contact page

---
+++ src/contact.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <main>
+                <h2>Feel free to Contact me below!</h2>
+            </main>
+        )
+    }
+}
+
+export default Contact;
