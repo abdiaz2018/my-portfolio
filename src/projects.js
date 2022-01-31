e471b2b2b4d9615071f7eafbc6c72ffdb90daf69
Abraham <abdiaz2018@gmail.com>
2022-01-30 19:50:56 -0800

    adding project

--- src/projects.js
+++ src/projects.js
@@ -18,10 +18,11 @@ class Projects extends Component {
                 </div>
 
                 <div class="project">
-                    <h3>Character CLI</h3>
+                    <h3>Force User API</h3>
                     <p>
-                        The first project that I made was a simple command line interface. I was able to scrape from an api of over 500 characters. I made it so that it would select 5 random characters for you to pick from by typing 1-5. From there it would get more information on the character you picked. 
-                        <a href="https://github.com/abdiaz2018/characterbioCLI" target="_blank" rel="noreferrer noopener">Here is the github.</a>
+                        This project was an api for force users that you can create. You can pick a name, add a race, force alignment, and force ability.
+                        <a href="https://github.com/abdiaz2018/the-force-js-frontend" target="_blank" rel="noreferrer noopener">Here is the frontend github.</a>
+                        <a href="https://github.com/abdiaz2018/the-force-js-backend" target="_blank" rel="noreferrer noopener">Here is the backend github.</a>
                     </p>
                 </div>
 
