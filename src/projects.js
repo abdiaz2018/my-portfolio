2493e153e3b610992ea51540810f1d1a01d13cba
Abraham <abdiaz2018@gmail.com>
2022-01-30 19:18:20 -0800

    adding project

--- src/projects.js
+++ src/projects.js
@@ -33,6 +33,21 @@ class Projects extends Component {
                     </p>
                 </div>
 
+                <div class="project">
+                    <h3>Create a Transformers</h3>
+                    <p>
+                        This project was a little app where you can log in and create transformers in a database. You can add a name ,alternate form, and choose to be an autobot or decepticon. 
+                        <a href="https://github.com/abdiaz2018/create-a-transformer" target="_blank" rel="noreferrer noopener">Here is the github.</a>
+                    </p>
+                </div>
+                
+                <div class="project">
+                    <h3>Character CLI</h3>
+                    <p>
+                        The first project that I made was a simple command line interface. I was able to scrape from an api of over 500 characters. I made it so that it would select 5 random characters for you to pick from by typing 1-5. From there it would get more information on the character you picked. 
+                        <a href="https://github.com/abdiaz2018/characterbioCLI" target="_blank" rel="noreferrer noopener">Here is the github.</a>
+                    </p>
+                </div>
             </main>
         )
     }
