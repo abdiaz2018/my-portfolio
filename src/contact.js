d82cfe3483b501347be2330a1244a6b2a9450c20
Abraham <abdiaz2018@gmail.com>
2022-02-04 20:15:18 -0800

    finished link

--- src/contact.js
+++ src/contact.js
@@ -5,7 +5,7 @@ class Contact extends Component {
         return (
             <main>
                 <h2>Interested in contacting me?</h2>
-                <p>Use this <a>link</a> to leave your contact info and I will try to contact you as soon as possible. Thank you for considering me.</p>
+                <p>Use this <a href="https://forms.gle/85kH7jVfNvLzHnTv5">link</a> to leave your contact info and I will try to contact you as soon as possible. Thank you for considering me.</p>
             </main>
         )
     }
