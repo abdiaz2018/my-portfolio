c9af5ccf342cbd0a1ab71b50648ce578093f3ede
Abraham <abdiaz2018@gmail.com>
2022-02-13 22:44:26 -0800

    fixing project

--- src/projects.js
+++ src/projects.js
@@ -43,7 +43,13 @@ class Projects extends Component {
                     </p>
                 </div>
                 
-               
+                <div class="project">
+                    <h3>Character CLI</h3>
+                    <p>
+                        The first project that I made was a simple command line interface. I was able to scrape from an api of over 500 characters. I made it so that it would select 5 random characters for you to pick from by typing 1-5. From there it would get more information on the character you picked. 
+                        <a href="https://github.com/abdiaz2018/characterbioCLI" target="_blank" rel="noreferrer noopener">Here is the github.</a>
+                    </p>
+                </div>
             </main>
         )
     }
