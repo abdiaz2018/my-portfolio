8a0be069a120a512fe47de7d93b0a053c518b5f9
Abraham <abdiaz2018@gmail.com>
2022-03-06 23:57:50 -0800

    a

--- src/home.js
+++ src/home.js
@@ -4,10 +4,10 @@ class Home extends Component {
     render() {
         return (
             <main>
-                <h2>Hi, I'm Abraham</h2>
+               
                 <p>
                     I am a Software Engineer and I just graduated from Flatiron School. I grew up in San Jose, California. I am proficient in Ruby, Rails, Javascript, and React. I would love to show you what I know and become an even better Software Engineer. I hope to be a Frontend Developer or a Fullstack Developer. Please check out my projects that I created in school and anything I am currently working on!
-                </p>
+                </p> 
             </main>
         )
     }
