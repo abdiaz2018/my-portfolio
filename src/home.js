6f449eea3c0cf098ec4389afe74b1b9e3f6780f3
Abraham <abdiaz2018@gmail.com>
2022-04-03 21:47:01 -0700

    fixing formatting

--- src/home.js
+++ src/home.js
@@ -10,7 +10,8 @@ class Home extends Component {
                     I grew up in San Jose, California. 
                     I always wanted to make applications for people to use.
                     I hope I am able to do that as a developer.
-                    I am proficient in Ruby, Rails, Javascript, and React. I would love to show you what I know and become an even better Software Engineer. I hope to be a Frontend Developer or a Fullstack Developer. I also would like to make video games as a hobby. I always enjoyed video games and would love to give the same feeling I felt as a child playing video games. Please check out my projects that I created in school and anything I am currently working on!
+                    I am proficient in Ruby, Rails, Javascript, and React. 
+                    I would love to show you what I know and become an even better Software Engineer. I hope to be a Frontend Developer or a Fullstack Developer. I also would like to make video games as a hobby. I always enjoyed video games and would love to give the same feeling I felt as a child playing video games. Please check out my projects that I created in school and anything I am currently working on!
                 </p> 
             </main>
         )
